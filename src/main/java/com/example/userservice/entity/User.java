0e8be7c65e26c052e52174abc9c2563e5256dfbd
Andrey_Berg <berg.andrey@icloud.com>
2025-11-17 20:15:40 +0300

    Added User entity

---
+++ src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,52 @@
+package com.example.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users", indexes = {
+        @Index(name = "idx_user_email", columnList = "email"),
+        @Index(name = "idx_user_active", columnList = "active")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(unique = true)
+    private String email;
+
+    private Boolean active = true;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<PaymentCard> paymentCards = new ArrayList<>();
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
