cb9e1c221b3251a19a5b2e3b4abc1134dd6eb23c
Andrey_Berg <berg.andrey@icloud.com>
2025-11-18 20:01:21 +0300

    add UserRequestDTO

---
+++ src/main/java/com/example/userservice/dto/UserRequestDTO.java
@@ -0,0 +1,29 @@
+package com.example.userservice.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UserRequestDTO {
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 1, max = 255, message = "Name must be between 1 and 255 characters")
+    private String name;
+
+    @NotBlank(message = "Surname is mandatory")
+    @Size(min = 1, max = 255, message = "Surname must be between 1 and 255 characters")
+    private String surname;
+
+    @Past(message = "Birth date must be in the past")
+    private LocalDate birthDate;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Size(max = 255, message = "Email must not exceed 255 characters")
+    private String email;
+
+    private Boolean active = true;
+}
