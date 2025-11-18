d741de05c572240a19bfa25f286b5d065ac78cdc
Andrey_Berg <berg.andrey@icloud.com>
2025-11-18 20:02:00 +0300

    add UserMapper

---
+++ src/main/java/com/example/userservice/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.example.userservice.mapper;
+
+import com.example.userservice.dto.UserRequestDTO;
+import com.example.userservice.dto.UserResponseDTO;
+import com.example.userservice.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "paymentCards", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    User toEntity(UserRequestDTO userRequestDTO);
+
+    UserResponseDTO toDTO(User user);
+}
