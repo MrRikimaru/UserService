96575d5f1118febb71958b2b30ba9628942c04a7
Andrey_Berg <berg.andrey@icloud.com>
2025-11-18 20:43:07 +0300

    add DuplicateCardNumberException

---
+++ src/main/java/com/example/userservice/exception/DuplicateCardNumberException.java
@@ -0,0 +1,7 @@
+package com.example.userservice.exception;
+
+public class DuplicateCardNumberException extends RuntimeException {
+    public DuplicateCardNumberException(String message) {
+        super(message);
+    }
+}
