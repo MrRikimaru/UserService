19668cb5c00cb3ef4431cd38871641b826bb6f7f
Andrey_Berg <berg.andrey@icloud.com>
2025-11-18 18:03:48 +0300

    add UserRepository

---
+++ src/main/java/com/example/userservice/repository/UserRepository.java
@@ -0,0 +1,34 @@
+package com.example.userservice.repository;
+
+import com.example.userservice.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByEmail(String email);
+
+    Page<User> findByActiveTrue(Pageable pageable);
+
+    boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name% AND u.surname LIKE %:surname%")
+    Page<User> findByNameAndSurnameContaining(@Param("name") String name, @Param("surname") String surname, Pageable pageable);
+
+    @Query(value = "SELECT * FROM users u WHERE u.active = :active AND u.birth_date < :birthDate", nativeQuery = true)
+    Page<User> findActiveUsersBornBefore(@Param("birthDate") LocalDate birthDate, @Param("active") boolean active, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE User u SET u.active = :active WHERE u.id = :id")
+    void updateActiveStatus(@Param("id") Long id, @Param("active") Boolean active);
+}
